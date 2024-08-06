bfa2a8dc97db2d013e86a533f28e96e1de1213aa
George-Mikulich <g.mikulich@softteco.com>
2024-08-06 12:30:51 +0300

    mess

--- setup.sh
+++
@@ -1,5 +0,0 @@
-helm repo add spinnaker https://opsmx.github.io/spinnaker-helm/
-helm repo update
-
-helm install spinnaker spinnaker/spinnaker -n spinnaker --create-namespace
-kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=spinnaker -n spinnaker
