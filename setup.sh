2082f98afc4c234ab7b47dacd5f075c304b8165c
George-Mikulich <g.mikulich@softteco.com>
2024-08-07 10:12:05 +0300

    timeout

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ helm repo add litmuschaos https://litmuschaos.github.io/litmus-helm/
 helm repo update
 
 helm install chaos litmuschaos/litmus --namespace=litmus --set portal.frontend.service.type=NodePort --create-namespace
-kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=chaos -n litmus
+kubectl wait --for=condition=Ready pod -l app.kubernetes.io/instance=chaos -n litmus --timeout 600s
 
 kubectl apply -f https://raw.githubusercontent.com/litmuschaos/litmus/master/mkdocs/docs/3.6.1/litmus-portal-crds-3.6.1.yml
 
